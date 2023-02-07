e44699bb17e7a7481199d3deaaa7caf4bf20f329
seayu <yhyuhai13@163.com>
2023-02-07 17:28:14 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <main><RouterView /></main>
+</template>
+<script lang='ts' setup>
+</script>
+<style scoped lang='less'></style>
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    vue(),
+    // https://github.com/hannoeru/vite-plugin-pages
+    Pages(),
+    // https://github.com/antfu/vite-plugin-components
+    Components({
+      dts: true,
+    }),
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        'vue/macros',
+        'vue-router',
+        '@vueuse/core',
+      ],
+      dts: true,
+      dirs: [],
+      vueTemplate: true,
+    }),
+  ],
+})
